b19a6766a9d569d25a19ce0efb633e988b4e219a
ErikCsobor <csobor.erik@gmail.com>
2022-12-08 13:30:51 +0100

    React assets
    React hozzárendelése főoldal váza

--- Webes alkalmazas/resources/js/app.js
+++ Webes alkalmazas/resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+import ReactDom from 'react-dom/client';
+import App from './components/App';
+const app = document.getElementById("app")
+if(app){
+    const root = ReactDom.createRoot(app);
+    root.render(
+        < App />
+    )
+}
+
