d9a18a7537dba3e98ebdd67086734fec680914c2
ErikCsobor <csobor.erik@gmail.com>
2022-12-10 18:24:54 +0100

    LS23-8 Customers rész kész
    Vásárlók rész kész

---
+++ Asztali alkalmazas/UI/UserControls/Customer_UC.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Asztali_alkalmazas.UI.UserControls
+{
+    partial class Customer_UC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CustomersDGV = new System.Windows.Forms.DataGridView();
+            this.customerPhone = new System.Windows.Forms.TextBox();
+            this.customerFNTB = new System.Windows.Forms.TextBox();
+            this.customerLNTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customerUpdateBT = new System.Windows.Forms.Button();
+            this.customerNewBT = new System.Windows.Forms.Button();
+            this.customerDeleteBT = new System.Windows.Forms.Button();
+            this.getCustomerOrderBT = new System.Windows.Forms.Button();
+            this.userProfilBT = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Akashi_hun", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 31);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Vásárlók adatai:";
+            // 
+            // CustomersDGV
+            // 
+            this.CustomersDGV.AllowUserToAddRows = false;
+            this.CustomersDGV.AllowUserToDeleteRows = false;
+            this.CustomersDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.CustomersDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersDGV.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CustomersDGV.Location = new System.Drawing.Point(0, 455);
+            this.CustomersDGV.Name = "CustomersDGV";
+            this.CustomersDGV.ReadOnly = true;
+            this.CustomersDGV.Size = new System.Drawing.Size(1454, 393);
+            this.CustomersDGV.TabIndex = 20;
+            this.CustomersDGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CustomersDGV_CellClick);
+            // 
+            // customerPhone
+            // 
+            this.customerPhone.Location = new System.Drawing.Point(1046, 145);
+            this.customerPhone.Name = "customerPhone";
+            this.customerPhone.Size = new System.Drawing.Size(194, 20);
+            this.customerPhone.TabIndex = 26;
+            this.customerPhone.Leave += new System.EventHandler(this.customerPhone_Leave);
+            // 
+            // customerFNTB
+            // 
+            this.customerFNTB.Location = new System.Drawing.Point(670, 145);
+            this.customerFNTB.Name = "customerFNTB";
+            this.customerFNTB.Size = new System.Drawing.Size(194, 20);
+            this.customerFNTB.TabIndex = 25;
+            this.customerFNTB.Leave += new System.EventHandler(this.customerFNTB_Leave);
+            // 
+            // customerLNTB
+            // 
+            this.customerLNTB.Location = new System.Drawing.Point(277, 145);
+            this.customerLNTB.Name = "customerLNTB";
+            this.customerLNTB.Size = new System.Drawing.Size(194, 20);
+            this.customerLNTB.TabIndex = 24;
+            this.customerLNTB.Leave += new System.EventHandler(this.customerLNTB_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Akashi_hun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(983, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 19);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Mobilszám:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Akashi_hun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(594, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 19);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Keresztnév:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Akashi_hun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(202, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 19);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Vezetéknév:";
+            // 
+            // customerUpdateBT
+            // 
+            this.customerUpdateBT.Enabled = false;
+            this.customerUpdateBT.Font = new System.Drawing.Font("Akashi_hun", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerUpdateBT.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.customerUpdateBT.Location = new System.Drawing.Point(541, 314);
+            this.customerUpdateBT.Name = "customerUpdateBT";
+            this.customerUpdateBT.Size = new System.Drawing.Size(229, 38);
+            this.customerUpdateBT.TabIndex = 27;
+            this.customerUpdateBT.Text = "Szerkeztés";
+            this.customerUpdateBT.UseVisualStyleBackColor = true;
+            this.customerUpdateBT.Click += new System.EventHandler(this.customerUpdateBT_Click);
+            // 
+            // customerNewBT
+            // 
+            this.customerNewBT.Font = new System.Drawing.Font("Akashi_hun", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerNewBT.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.customerNewBT.Location = new System.Drawing.Point(73, 314);
+            this.customerNewBT.Name = "customerNewBT";
+            this.customerNewBT.Size = new System.Drawing.Size(229, 38);
+            this.customerNewBT.TabIndex = 28;
+            this.customerNewBT.Text = "Új";
+            this.customerNewBT.UseVisualStyleBackColor = true;
+            this.customerNewBT.Click += new System.EventHandler(this.customerNewBT_Click);
+            // 
+            // customerDeleteBT
+            // 
+            this.customerDeleteBT.Font = new System.Drawing.Font("Akashi_hun", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerDeleteBT.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.customerDeleteBT.Location = new System.Drawing.Point(1022, 314);
+            this.customerDeleteBT.Name = "customerDeleteBT";
+            this.customerDeleteBT.Size = new System.Drawing.Size(229, 38);
+            this.customerDeleteBT.TabIndex = 29;
+            this.customerDeleteBT.Text = "Törlés";
+            this.customerDeleteBT.UseVisualStyleBackColor = true;
+            this.customerDeleteBT.Click += new System.EventHandler(this.customerDeleteBT_Click);
+            // 
+            // getCustomerOrderBT
+            // 
+            this.getCustomerOrderBT.Enabled = false;
+            this.getCustomerOrderBT.Font = new System.Drawing.Font("Akashi_hun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getCustomerOrderBT.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.getCustomerOrderBT.Location = new System.Drawing.Point(942, 23);
+            this.getCustomerOrderBT.Name = "getCustomerOrderBT";
+            this.getCustomerOrderBT.Size = new System.Drawing.Size(499, 38);
+            this.getCustomerOrderBT.TabIndex = 30;
+            this.getCustomerOrderBT.Text = "Kijelölt vásárló vásárlási előzményének megjelenítése";
+            this.getCustomerOrderBT.UseVisualStyleBackColor = true;
+            this.getCustomerOrderBT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // userProfilBT
+            // 
+            this.userProfilBT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.userProfilBT.BackgroundImage = global::Asztali_alkalmazas.Properties.Resources.icons8_reset_50px_1;
+            this.userProfilBT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.userProfilBT.FlatAppearance.BorderSize = 0;
+            this.userProfilBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.userProfilBT.Location = new System.Drawing.Point(1408, 412);
+            this.userProfilBT.Name = "userProfilBT";
+            this.userProfilBT.Size = new System.Drawing.Size(33, 28);
+            this.userProfilBT.TabIndex = 31;
+            this.userProfilBT.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.userProfilBT.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.userProfilBT.UseVisualStyleBackColor = true;
+            this.userProfilBT.Click += new System.EventHandler(this.userProfilBT_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(1254, 420);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 16);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Adatbázis újratöltése:";
+            // 
+            // Customer_UC
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(57)))));
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.userProfilBT);
+            this.Controls.Add(this.getCustomerOrderBT);
+            this.Controls.Add(this.customerDeleteBT);
+            this.Controls.Add(this.customerNewBT);
+            this.Controls.Add(this.customerUpdateBT);
+            this.Controls.Add(this.customerPhone);
+            this.Controls.Add(this.customerFNTB);
+            this.Controls.Add(this.customerLNTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CustomersDGV);
+            this.Controls.Add(this.label1);
+            this.Name = "Customer_UC";
+            this.Size = new System.Drawing.Size(1454, 848);
+            this.Load += new System.EventHandler(this.Customer_UC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView CustomersDGV;
+        private System.Windows.Forms.TextBox customerPhone;
+        private System.Windows.Forms.TextBox customerFNTB;
+        private System.Windows.Forms.TextBox customerLNTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button customerUpdateBT;
+        private System.Windows.Forms.Button customerNewBT;
+        private System.Windows.Forms.Button customerDeleteBT;
+        private System.Windows.Forms.Button getCustomerOrderBT;
+        private System.Windows.Forms.Button userProfilBT;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Asztali alkalmazas/UI/UserControls/Customer_UC.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Asztali_alkalmazas.Classes;
+using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Asztali_alkalmazas.UI.UserControls
+{
+    public partial class Customer_UC : UserControl
+    {
+        MySqlConnection conn;
+        string format = "yyyy-MM-dd HH:mm:ss";
+        int cID;
+        string connstring;
+        string currentId;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+        public Customer_UC()
+        {
+            InitializeComponent();
+            connstring = "server=localhost;port=3306;database=local_store_project_23;user=root;";
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = connstring;
+                conn.Open();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+            getID();//Az adatbázisban következő ID-t adja az új felhasználónak           
+            uj.setId(cID);
+        }
+        Customer uj = new Customer();
+        AdminControl_UC hibakezeles = new AdminControl_UC();
+
+        private void getID()
+        {
+            conn.Open();
+            cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT id FROM local_store_project_23.customers order by id desc ";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                string ideig = dr["id"].ToString();
+                cID = int.Parse(ideig);
+            }
+            conn.Close();
+        }
+
+        private DataTable GetCustomersList()
+        {
+            DataTable dtCustomers = new DataTable();
+            conn.Open();
+            cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT customers.id as ID, First_name as 'Keresztnev', Last_name as 'Vezeteknev', Phone as 'Mobilszam' FROM local_store_project_23.customers order by id asc;";
+            dr = cmd.ExecuteReader();
+            dtCustomers.Load(dr);
+            conn.Close();
+            return dtCustomers;
+        }
+        private DataTable GetCustomerOrder()
+        {
+            DataTable dtCustomerOrder = new DataTable();
+            conn.Open();
+            cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT customers.First_name as 'Keresztnev', customers.Last_name as 'Vezeteknev', OrderDate as 'Rendeles datuma', OrderNumber as 'Rendeles szama', TotalAmount as 'Vegosszeg(Ft)' FROM local_store_project_23.orders,customers\r\nwhere orders.CustomerId = customers.id and CustomerId = @ID;";
+            cmd.Parameters.AddWithValue("ID", currentId);
+            dr = cmd.ExecuteReader();
+            dtCustomerOrder.Load(dr);
+            conn.Close();
+            return dtCustomerOrder;
+        }
+
+        private void ReLoadDGV()
+        {
+            CustomersDGV.DataSource = null;
+            CustomersDGV.DataSource = GetCustomersList();
+        }
+        private void setNewCustomer()
+        {
+            
+            try
+            {
+                conn.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO `local_store_project_23`.`customers`(`First_name`, `Last_name`, `Phone`, created_at, updated_at)  VALUES ('" + uj.First_name + "', '" + uj.Last_name + "', '" + uj.Phone + "', '"+DateTime.Now.ToString(format)+"','"+DateTime.Now.ToString(format)+"')";
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Létrehozva!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hiba!" + ex.Message);
+                string hiba = ex.Message.ToString();
+                hibakezeles.ErrorLogs(hiba);
+
+            }
+            conn.Close();
+            ReLoadDGV();
+
+        }
+        private void deleteTB()
+        {
+            customerLNTB.Clear();
+            customerFNTB.Clear();
+            customerPhone.Clear();
+        }
+        private void Customer_UC_Load(object sender, EventArgs e)
+        {
+            ReLoadDGV();
+
+        }
+
+        private void CustomersDGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                    if (CustomersDGV.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+                    {
+                        CustomersDGV.CurrentRow.Selected = true;
+                        currentId = CustomersDGV.Rows[e.RowIndex].Cells["ID"].FormattedValue.ToString();
+                        customerFNTB.Text = CustomersDGV.Rows[e.RowIndex].Cells["Keresztnev"].FormattedValue.ToString();
+                        customerLNTB.Text = CustomersDGV.Rows[e.RowIndex].Cells["Vezeteknev"].FormattedValue.ToString();
+                        customerPhone.Text = CustomersDGV.Rows[e.RowIndex].Cells["Mobilszam"].FormattedValue.ToString();
+                    }
+                customerUpdateBT.Enabled = true;
+                getCustomerOrderBT.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = ex.Message.ToString();
+                
+                hibakezeles.ErrorLogs(hiba);
+            }
+        }
+
+        private void customerDeleteBT_Click(object sender, EventArgs e)
+        {
+            DialogResult torli;
+            torli = MessageBox.Show("Biztos törli az adott elemet?", "Törlés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (torli == DialogResult.Yes)
+            {
+                conn.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "delete from `local_store_project_23`.`customers` WHERE `id`= @ID;";
+                cmd.Parameters.AddWithValue("@ID", currentId);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                ReLoadDGV();
+            }
+        }
+
+        private void customerLNTB_Leave(object sender, EventArgs e)
+        {
+            if(customerLNTB.Text.Length > 0)
+            {
+                uj.setLastname(customerLNTB.Text);
+
+            }
+        }
+
+        private void customerFNTB_Leave(object sender, EventArgs e)
+        {
+            if(customerFNTB.Text.Length > 0)
+            {
+                uj.setFirstname(customerFNTB.Text);
+            }
+        }
+
+        private void customerPhone_Leave(object sender, EventArgs e)
+        {
+            if(customerPhone.Text.Length > 0)
+            {
+                uj.setPhoneNumber(customerPhone.Text);
+            }
+        }
+
+        private void customerNewBT_Click(object sender, EventArgs e)
+        {
+            setNewCustomer();
+            deleteTB();
+        }
+
+        private void customerUpdateBT_Click(object sender, EventArgs e)
+        {
+            if(uj.First_name != customerFNTB.Text || uj.Last_name != customerLNTB.Text || uj.Phone != customerPhone.Text)
+            {
+                uj.setFirstname(customerFNTB.Text);
+                uj.setLastname(customerLNTB.Text);
+                uj.setPhoneNumber(customerPhone.Text);
+                conn.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "UPDATE `local_store_project_23`.`customers` SET `First_name`='" + uj.First_name + "', Last_name = '" + uj.Last_name + "', Phone = '" + uj.Phone + "', updated_at = '" + DateTime.Now.ToString(format) + "' WHERE `id`= @ID;";
+                cmd.Parameters.AddWithValue("@ID", currentId);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                ReLoadDGV();
+            }
+            else
+            {
+                MessageBox.Show("Nem változtattál semmilyen adaton.");
+            }
+        }
+
+        private void userProfilBT_Click(object sender, EventArgs e)
+        {
+            ReLoadDGV();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomersDGV.DataSource = GetCustomerOrder();
+        }
+    }
+}
